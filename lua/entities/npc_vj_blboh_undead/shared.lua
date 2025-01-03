45148c93b320bdb7ea236188c2c8f4184fd8f0c4
Warkin Iskander Volselli <55023547+WarkinVolselli@users.noreply.github.com>
2025-01-03 17:20:40 -0500

    Undead

---
+++ lua/entities/npc_vj_blboh_undead/shared.lua
@@ -0,0 +1,8 @@
+ENT.Base 			= "npc_vj_creature_base"
+ENT.Type 			= "ai"
+ENT.PrintName 		= "Husk"
+ENT.Author 			= "Warkin"
+ENT.Contact 		= ""
+ENT.Purpose 		= "A defiled corpse."
+ENT.Instructions 	= "Put it back underground."
+ENT.Category		= "Bean's Little Box of Horrors"
