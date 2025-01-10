fb00686ac42ce347176ac85ddb444f72b3eba5e2
An average Beanmann <102012974+AnaverageBeanmann@users.noreply.github.com>
2025-01-09 22:27:54 -0500

    shepherd

--- lua/entities/obj_vj_blboh_shepherd_bullet.lua
+++ lua/entities/obj_vj_blboh_shepherd_bullet.lua
@@ -4,64 +4,44 @@ ENT.Type 			= "anim"
 ENT.Base 			= "obj_vj_projectile_base"
 ENT.PrintName		= "Shepherd's Bullet"
 ENT.Author 			= "DrVrej, modified by An average Beanmann"
-ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
-ENT.Information		= "Projectiles for my addons"
-ENT.Category		= "Bean's Half-Life Coop Infected Expansions"
+ENT.Contact 		= ""
+ENT.Information		= "bullet"
+ENT.Category		= "Bean's Little Box of Horrors"
 
 ENT.Spawnable = false
 ENT.AdminOnly = false
 --------------------
 if CLIENT then
-	language.Add("obj_shepherd_bullet", "Shepherd's Bullet")
-	killicon.Add("obj_shepherd_bullet","HUD/killicons/default",Color(255,80,0,255))
+	-- do we need this?
+	-- language.Add("obj_shepherd_bullet", "Shepherd's Bullet")
+	-- killicon.Add("obj_shepherd_bullet","HUD/killicons/default",Color(255,80,0,255))
 
-	language.Add("#obj_shepherd_bullet", "Shepherd's Bullet")
-	killicon.Add("#obj_shepherd_bullet","HUD/killicons/default",Color(255,80,0,255))
+	-- language.Add("#obj_shepherd_bullet", "Shepherd's Bullet")
+	-- killicon.Add("#obj_shepherd_bullet","HUD/killicons/default",Color(255,80,0,255))
 	
 	function ENT:Draw() self:DrawModel() end
 end
 --------------------
 if !SERVER then return end
 ENT.Model = {"models/vj_blboh/shepherd_bullet.mdl"}
-
-ENT.DoesDirectDamage = true -- Should it do a direct damage when it hits something?
--- ENT.DirectDamage = 20 -- How much damage should it do when it hits something
-ENT.DirectDamage = 50 -- How much damage should it do when it hits something
+--------------------
+ENT.DoesDirectDamage = true
+ENT.DirectDamage = 50
 ENT.DirectDamageType = DMG_BULLET
-
-ENT.SoundTbl_OnCollide = {"npc/shepherd/bullet_hit.mp3"}
-
--- ENT.ProjectileType = VJ.PROJ_TYPE_LINEAR 
--- ENT.BHLCIE_Shepherd_Difficulty = 1
+--------------------
+ENT.SoundTbl_OnCollide = {"vj_blboh/shepherd/bullet_hit.mp3"}
 --------------------
 function ENT:Init()
-
+	self:GetPhysicsObject():EnableGravity(false)
+	self:GetPhysicsObject():EnableDrag(false)
+	self:GetPhysicsObject():SetMass(1)
 	util.SpriteTrail(self,1,Color(255,255,0),false,3,0,0.1,1,"trails/smoke")
-
-	-- self.BHLCIE_Shepherd_Difficulty = GetConVar("hl1_coop_sv_skill"):GetInt()
-	-- if self.BHLCIE_Shepherd_Difficulty == 3 then
-		-- self.DirectDamage = 70
-	-- elseif self.BHLCIE_Shepherd_Difficulty == 2 then
-		-- self.DirectDamage = 50
-	-- end
-
+	-- phys:SetBuoyancyRatio(0)
 end
 --------------------
-function ENT:CustomPhysicsObjectOnInitialize(phys)
-	phys:Wake()
-	phys:SetMass(1)
-	phys:SetBuoyancyRatio(0)
-	phys:EnableDrag(false)
-	phys:EnableGravity(false)
-end
---------------------
--- function ENT:CustomOnCollideWithoutRemove(data, phys)
-	-- timer.Simple(0.001,function() if IsValid(self) then
-		-- self:Remove(self)
-	-- end end)
--- end -- Return false to disable the base functions from running
---------------------
--- function ENT:CustomOnDoDamage_Direct(data, phys, hitEnt)
 function ENT:OnDealDamage(data, phys, hitEnts)
-	VJ_EmitSound(hitEnt,"npc/shepherd/bullet_hit_target.mp3",60,100)
-end -- "hitEnts" = Table of entities damage or false if it hit nothing
+	if hitEnts != false then
+		self.HasOnCollideSounds = false
+		VJ.EmitSound(self,"vj_blboh/shepherd/bullet_hit_target.mp3",70,100)
+	end
+end
--- lua/weapons/weapon_vj_blboh_shepherd_gun/shared.lua
+++ lua/weapons/weapon_vj_blboh_shepherd_gun/shared.lua
@@ -1,60 +1,58 @@
 SWEP.Base = "weapon_vj_base"
 SWEP.PrintName = "The Shepherd's Gun"
 SWEP.Author = "An average Beanmann"
+SWEP.Contact = ""
 SWEP.Purpose = "A tool for tending to your flock."
 SWEP.Instructions = "Pull the trigger."
 SWEP.Category = "Bean's Little Box of Horrors"
 SWEP.MadeForNPCsOnly = true
 SWEP.HoldType = "shotgun"
----------------------------------------------------------------------------------------------------------------------------------------------
+--------------------
 SWEP.WorldModel = "models/vj_blboh/shepherd_gun.mdl"
-	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.NPC_NextPrimaryFire = 2 -- Next time it can use primary fire
-SWEP.NPC_CustomSpread = 0.2 -- This is added on top of the custom spread that's set inside the SNPC! | Starting from 1: Closer to 0 = better accuracy, Farther than 1 = worse accuracy
-SWEP.NPC_BulletSpawnAttachment = "0" -- The attachment that the bullet spawns on, leave empty for base to decide!
-	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.Primary.DisableBulletCode	= true -- The bullet won't spawn, this can be used when creating a projectile-based weapon
-SWEP.Primary.ClipSize = 1 -- Max amount of bullets per clip
-SWEP.Primary.Sound = {"npc/shepherd/shoot.mp3"}
-SWEP.Primary.DistantSound = {"npc/shepherd/shoot.mp3"}
-SWEP.Primary.DistantSoundVolume	= 0.7 -- Distant sound volume
-SWEP.PrimaryEffects_ShellType = "VJ_Weapon_ShotgunShell1"
-SWEP.Reload_TimeUntilAmmoIsSet = 5
-
-SWEP.HasDryFireSound = false
+--------------------
+SWEP.NPC_NextPrimaryFire = 2
+SWEP.NPC_CustomSpread = 0.2
+SWEP.NPC_BulletSpawnAttachment = "0"
 SWEP.NPC_HasReloadSound = false
-
-SWEP.BHLCIE_Shepherd_Difficulty = 1
-SWEP.BHLCIE_Shepherd_BulletSpeed = 500
----------------------------------------------------------------------------------------------------------------------------------------------
-function SWEP:Init()
-	-- self.BHLCIE_Shepherd_Difficulty = GetConVar("hl1_coop_sv_skill"):GetInt()
-	-- if self.BHLCIE_Shepherd_Difficulty == 3 then
-		-- self.BHLCIE_Shepherd_BulletSpeed = 1500
-	-- elseif self.BHLCIE_Shepherd_Difficulty == 2 then
-		-- self.BHLCIE_Shepherd_BulletSpeed = 1000
-	-- end
-end
----------------------------------------------------------------------------------------------------------------------------------------------
+--------------------
+SWEP.Primary.DisableBulletCode = true
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.Sound = {"vj_blboh/shepherd/shoot.mp3"}
+SWEP.Primary.DistantSound = {"vj_blboh/shepherd/shoot.mp3"}
+SWEP.PrimaryEffects_MuzzleAttachment = "0"
+-- SWEP.PrimaryEffects_ShellType = "VJ_Weapon_ShotgunShell1"
+SWEP.PrimaryEffects_ShellType = "ShotgunShellEject"
+--------------------
+SWEP.Reload_TimeUntilAmmoIsSet = 5
+--------------------
+SWEP.HasDryFireSound = false
+--------------------
 function SWEP:OnPrimaryAttack(status, statusData)
-	if status "Initial" then
+	if status == "Initial" then
 		if CLIENT then return end
-		local proj = ents.Create("obj_shepherd_bullet")
-		local ply_Ang = self:GetOwner():GetAimVector():Angle()
-		local ply_Pos = self:GetOwner():GetShootPos()
-		if self:GetOwner():IsPlayer() then proj:SetPos(ply_Pos) else proj:SetPos(self:GetNW2Vector("VJ_CurBulletPos")) end
-		if self:GetOwner():IsPlayer() then proj:SetAngles(ply_Ang) else proj:SetAngles(self:GetOwner():GetAngles()) end
-		proj:SetOwner(self:GetOwner())
-		proj:Activate()
-		proj:Spawn()
+		local owner = self:GetOwner()
+		local projectile = ents.Create("obj_vj_blboh_shepherd_bullet")
+		-- local projectile = ents.Create("obj_vj_flareround")
+		local spawnPos = self:GetBulletPos()
+		if owner:IsPlayer() then
+			projectile:SetPos(owner:GetShootPos())
+		else
+			projectile:SetPos(spawnPos)
+		end
+		projectile:SetOwner(owner)
+		projectile:Activate()
+		projectile:Spawn()
 		
-		local phys = proj:GetPhysicsObject()
+		local phys = projectile:GetPhysicsObject()
 		if IsValid(phys) then
-			if self:GetOwner():IsPlayer() then
-				phys:SetVelocity(self:GetOwner():GetAimVector() * self.BHLCIE_Shepherd_BulletSpeed)
+			if owner.IsVJBaseSNPC then
+				phys:SetVelocity(owner:CalculateProjectile("Line", spawnPos, owner:GetAimPosition(owner:GetEnemy(), spawnPos, 1, 1750), 1750))
+			elseif owner:IsPlayer() then
+				phys:SetVelocity(owner:GetAimVector() * 1750)
 			else
-				phys:SetVelocity(self:GetOwner():CalculateProjectile("Line", self:GetNW2Vector("VJ_CurBulletPos"), self:GetOwner():GetEnemy():GetPos() + self:GetOwner():GetEnemy():OBBCenter(), self.BHLCIE_Shepherd_BulletSpeed))
+				phys:SetVelocity(owner:CalculateProjectile("Line", spawnPos, owner:GetEnemy():GetPos() + owner:GetEnemy():OBBCenter(), 1750))
 			end
+			projectile:SetAngles(projectile:GetVelocity():GetNormal():Angle())
 		end
 	end
 end
